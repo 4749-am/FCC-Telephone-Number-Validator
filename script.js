6c9636287a39b3e078e7e6376b23fbf00bc182a9
Anne-Marie M <amm47home@gmail.com>
2025-06-23 12:06:17 +0200

    clear button set

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ const result = document.getElementById("results-div");
 const checkButton = document.getElementById("check-btn");
 const clearButton = document.getElementById("clear-btn");
 
-checkButton.addEventListener("click", errorMessage);
+checkButton.addEventListener("click", function() {
+  errorMessage();
+  isUSPhoneNumber();
+});
+
+clearButton.addEventListener("click", reset);
+
+function reset() {
+    userInput.value = "";
+    result.innerText = "";
+}
 
 userInput.addEventListener("keydown", function(event) {
   if (event.key === "Enter") {
@@ -13,8 +23,18 @@ userInput.addEventListener("keydown", function(event) {
 
 function errorMessage() {
   if (userInput.value === "") {
-    alert("Please provide a phone number")
+    alert("Please provide a phone number");
+    return true;
   }
-  return;
+  return false;
 };
 
+function isUSPhoneNumber() {
+  if(/^\\(:([0-9]{3})\\)?[-.\\s]?([0-9]{3})[-.\\s]?([0-9]{4})$/.test(userInput.value)) {
+    result.innerText = `Valid US number: ${userInput.value}`
+  }
+  else {
+    result.innerText = `Invalid US number: ${userInput.value}`
+  }
+  return;
+}
